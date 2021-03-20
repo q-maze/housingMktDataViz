1d80adf01f945706d8af6fff92b701d3a2104f80
Quinton <50850722+q-maze@users.noreply.github.com>
2021-03-20 17:41:28 -0400

    Delete dataExploration.py
    not needed anymore

--- dataExploration.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-input_df = pd.read_csv("Location_Affordability_Index_v_1.0.csv")
-by_state = input_df.groupby('SF1_BlockGroups_STATE_NAME').mean()
-
-# df = by_state['residential_density'].mean().to_frame(name='mean_res_dens').reset_index()
-
-print(by_state.columns.tolist())
