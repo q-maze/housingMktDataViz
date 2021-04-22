5567572fb2f3dd46204a6d508a202e1d591b4503
Quinton <50850722+q-maze@users.noreply.github.com>
2021-04-22 19:56:56 -0400

    Update LocationAffordabilityIndex.py

--- LocationAffordabilityIndex.py
+++ LocationAffordabilityIndex.py
@@ -1,4 +1,7 @@
 import pandas as pd  # for storing the data
+import laiuser
+import numpy as np
+
 
 # class for representing a data object containing all aspects of Location Affordability Index (LAI) data
 # contains methods for merging county names from Federal Information Processing Standards (FIPS) dataset
@@ -22,12 +25,13 @@ class LocationAffordabilityIndex:
         self.df['BlockGroups_COUNTYFP10'] = self.df['BlockGroups_COUNTYFP10'].apply(lambda x: x.zfill(3))
         self.df['BlockGroups_STATEFP10'] = self.df['BlockGroups_STATEFP10'].apply(lambda x: x.zfill(2))
         # then add a column concat of the state and county
-        self.df['State-County'] =  self.df['BlockGroups_STATEFP10'] + self.df['BlockGroups_COUNTYFP10']
+        self.df['State-County'] = self.df['BlockGroups_STATEFP10'] + self.df['BlockGroups_COUNTYFP10']
 
         # merge the two data sets
         fips = self.get_county_names()
         self.df = self.df.merge(fips, how='left',
                                 on='State-County')
+        self.user = None
 
     # function to output current main dataframe to a csv file in the local directory
     def to_csv(self, filename='LAI.csv'):
@@ -42,7 +46,7 @@ class LocationAffordabilityIndex:
         fips = self.get_county_names()
         fips.to_excel('all-geocodes-v2016.xlsx')
 
-    # Returns a dataframe countaining FIPS State-County Code and associated county name
+    # Returns a dataframe containing FIPS State-County Code and associated county name
     def get_county_names(self):
         # read in the excel file from the supplied location or if none supplied, Census URL
         fips_df = pd.read_excel(self.fips_data_file_path,
@@ -64,6 +68,32 @@ class LocationAffordabilityIndex:
         fips_df = fips_df[['State-County', 'Area_Name']].copy()
         return fips_df
 
+    #  type 1 - Typical HH - "Median Household" > 80%, < 150%
+    #  type 2 - Moderate HH - < 80%, > 50%
+    #  type 3 - Dual Income HH - > 150%
+    #  type 4 - Low Income HH - < 50%
+    #  type 5 - Single Very Low Income  < 11880
+    #  type 6 - Single Professional HH - > 135%
+    #  type 7 - Single Worker HH - < 50%
+    #  type 8 - Retirees - 80% Median
+    def add_user_prof(self, user):
+        self.user = user
+        self.df['classification'] = 'type_8'
+
+        if user.classification == 'single':
+            if self.user.income < 11880:
+                self.df['classification'] = 'type_5'
+            else:
+                self.df['classification'] = np.where(self.df['per_capita_income'] > self.user.income * (1 / 1.35),
+                                                     'type_6')
+                self.df['classification'] = np.where(self.df['per_capita_income'] < self.user.income * (1 / 1.35),
+                                                     'type_7')
+        else:
+            self.df['classification'] = np.where(self.df['per_capita_income'] > self.user.income * (1 / 0.5),
+                                                 'type_4')
+            self.df['classification'] = np.where(self.user.income * (1 / 0.5) > self.df['per_capita_income'] > self.user.income * (1 / 0.8), 'type_3')
+            self.df['classification'] = np.where(self.df['per_capita_income'] > self.user.income * 1.5, 'type_2')
+
 
 if __name__ == "__main__":
     # THIS CODE TAKES A LONG TIME TO EXECUTE IF INTERNET SPEED ISN'T GOOD (> 1MIN)
@@ -74,4 +104,3 @@ if __name__ == "__main__":
     data.to_csv()
     data.to_smaller_csv()
     data.store_fips()
-
